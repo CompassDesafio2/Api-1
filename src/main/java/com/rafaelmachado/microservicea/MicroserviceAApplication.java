6bb0cdc7fcdcf141ade4885b01bca7a14d17a7e6
xandy <>
2025-02-07 16:47:03 -0400

    /fix: Removendo import não usados do MicroserviceAAplication

--- src/main/java/com/rafaelmachado/microservicea/MicroserviceAApplication.java
+++ src/main/java/com/rafaelmachado/microservicea/MicroserviceAApplication.java
@@ -1,8 +1,5 @@
 package com.rafaelmachado.microservicea;
 
-import com.rafaelmachado.microservicea.feing.PostagemClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
